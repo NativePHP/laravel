035fae0577ef27850fac2a5890e493147df0fdca
mpociot <mpociot@users.noreply.github.com>
2023-05-13 09:58:01 +0000

    Fix styling

--- src/Events/App/ApplicationBooted.php
+++ src/Events/App/ApplicationBooted.php
@@ -2,9 +2,7 @@
 
 namespace Native\Laravel\Events\App;
 
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
