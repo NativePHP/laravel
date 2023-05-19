7c6b496f21961919c00b5cb536b03341dec5d39b
Simon Hamp <simon.hamp@me.com>
2023-05-19 15:30:33 +0100

    Add route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Native\Laravel\Http\Controllers\CreateSecurityCookieController;
 use Native\Laravel\Http\Controllers\DispatchEventFromAppController;
 use Native\Laravel\Http\Controllers\NativeAppBootedController;
 use Native\Laravel\Http\Middleware\PreventRegularBrowserAccess;
@@ -9,3 +10,5 @@ Route::group(['middleware' => PreventRegularBrowserAccess::class], function () {
     Route::post('_native/api/booted', NativeAppBootedController::class);
     Route::post('_native/api/events', DispatchEventFromAppController::class);
 })->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+
+Route::get('_native/api/cookie', CreateSecurityCookieController::class);
