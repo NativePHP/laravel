a3956feb5e5918acea4ec89a86209aaf74559cd7
Marcel Pociot <m.pociot@gmail.com>
2023-05-25 15:02:05 +0200

    Add app_id, deeplink_scheme, author to config. (46)

--- config/native-php.php
+++ config/native-php.php
@@ -7,7 +7,15 @@ return [
 
     'database_path' => env('NATIVE_PHP_DATABASE_PATH'),
 
+    'version' => env('NATIVE_PHP_VERSION', '1.0.0'),
+
     'secret' => env('NATIVE_PHP_SECRET'),
+
+    'app_id' => env('NATIVEPHP_APP_ID'),
+
+    'deeplink_scheme' => env('NATIVEPHP_DEEPLINK_SCHEME'),
+
+    'author' => env('NATIVEPHP_APP_AUTHOR'),
 
     'api_url' => env('NATIVE_PHP_API_URL', 'http://localhost:4000/api/'),
 
