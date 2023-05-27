6436727c13868b4988a750dc01ac5732d3926933
Marcel Pociot <m.pociot@gmail.com>
2023-05-27 12:13:57 +0200

    Add updater configuration for Native PHP

--- config/native-php.php
+++ config/native-php.php
@@ -4,4 +4,31 @@ return [
     'hot_reload' => [
         base_path('app/Providers/NativeAppServiceProvider.php'),
     ],
+
+    'updater' => [
+        'enabled' => env('NATIVEPHP_UPDATER_ENABLED', true),
+
+        'default' => env('NATIVEPHP_UPDATER_PROVIDER', 'spaces'),
+
+        'providers' => [
+            's3' => [
+                'driver' => 's3',
+                'key' => env('AWS_ACCESS_KEY_ID'),
+                'secret' => env('AWS_SECRET_ACCESS_KEY'),
+                'region' => env('AWS_DEFAULT_REGION'),
+                'bucket' => env('AWS_BUCKET'),
+                'endpoint' => env('AWS_ENDPOINT'),
+                'path' => env('NATIVEPHP_UPDATER_PATH', null),
+            ],
+
+            'spaces' => [
+                'driver' => 'spaces',
+                'key' => env('DO_SPACES_KEY_ID'),
+                'secret' => env('DO_SPACES_SECRET_ACCESS_KEY'),
+                'name' => env('DO_SPACES_NAME'),
+                'region' => env('DO_SPACES_REGION'),
+                'path' => env('NATIVEPHP_UPDATER_PATH', null),
+            ]
+        ],
+    ],
 ];
