e56ab0adbe0490bf4d27fa098aafa95aeabcf614
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-21 00:44:01 +0330

    add test for abort

--- tests/Http/Controller/CreateSecurityCookieControllerTest.php
+++ tests/Http/Controller/CreateSecurityCookieControllerTest.php
@@ -8,3 +8,11 @@ it('create security cookie', function () {
     $this->assertEquals('localhost', $cookie->getDomain());
     $this->assertTrue($cookie->isHttpOnly());
 });
+
+it('check if secret is not equal of config secret key abort 403 page', function () {
+    config()->set('native-php.secret', 'milwad');
+
+    $response = $this->get('_native/api/cookie')->assertStatus(403);
+
+    $this->assertEquals([], $response->headers->getCookies());
+});
