277c2d1525d916a870c9909901e36931e23e142a
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-21 00:34:48 +0330

    add `create security cookie` test

--- tests/Http/Controller/CreateSecurityCookieControllerTest.php
+++ tests/Http/Controller/CreateSecurityCookieControllerTest.php
@@ -1,1 +1,10 @@
 <?php
+
+it('create security cookie', function () {
+    $response = $this->get('_native/api/cookie')->assertRedirect('/');
+    $cookie = $response->headers->getCookies()[0];
+
+    $this->assertEquals('_php_native', $cookie->getName());
+    $this->assertEquals('localhost', $cookie->getDomain());
+    $this->assertTrue($cookie->isHttpOnly());
+});
