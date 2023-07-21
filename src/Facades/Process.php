fb503ae6482fcc8011d40e14f59273a4dd36edac
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-21 13:15:54 +0330

    add Process methods into related facade

--- src/Facades/Process.php
+++ src/Facades/Process.php
@@ -4,6 +4,12 @@ namespace Native\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static string arch()
+ * @method static string platform()
+ * @method static float uptime()
+ * @method static object fresh()
+ */
 class Process extends Facade
 {
     protected static function getFacadeAccessor()
