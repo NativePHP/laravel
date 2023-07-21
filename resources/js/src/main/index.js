9232ce91d53fdb077617a1fa8d168846f958333e
hogus <hogus2037@gmail.com>
2023-07-21 14:19:28 +0800

    NativePHP package rename

--- resources/js/src/main/index.js
+++ resources/js/src/main/index.js
@@ -1,5 +1,5 @@
 import {app} from 'electron'
-import NativePHP from 'nativephp-electron'
+import NativePHP from '@nativephp/electron-plugin'
 import defaultIcon from '../../resources/icon.png?asset&asarUnpack'
 import phpBinary from '../../resources/php?asset&asarUnpack'
 import certificate from '../../resources/cacert.pem?asset&asarUnpack'
