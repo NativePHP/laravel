77eafb3d79c5daaf058c8dbbf8d90e75f91f26d1
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-21 13:12:19 +0330

    add Dock methods into related facade

--- src/Facades/Dock.php
+++ src/Facades/Dock.php
@@ -3,7 +3,11 @@
 namespace Native\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use Native\Laravel\Menu\Menu;
 
+/**
+ * @method static void menu(Menu $menu)
+ */
 class Dock extends Facade
 {
     protected static function getFacadeAccessor()
