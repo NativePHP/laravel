3961d3728e3bdb797795b4c660bd9ff9a464c9d4
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-21 13:13:17 +0330

    add GlobalShortcut methods into related facade

--- src/Facades/GlobalShortcut.php
+++ src/Facades/GlobalShortcut.php
@@ -4,6 +4,12 @@ namespace Native\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static self key(string $key)
+ * @method static self event(string $event)
+ * @method static void register()
+ * @method static void unregister()
+ */
 class GlobalShortcut extends Facade
 {
     protected static function getFacadeAccessor()
