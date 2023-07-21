64df08a386c11a079257f7be7f7260fb84eb1ec4
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-07-21 13:14:55 +0330

    add MenuBar methods into related facade

--- src/Facades/MenuBar.php
+++ src/Facades/MenuBar.php
@@ -4,6 +4,12 @@ namespace Native\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \Native\Laravel\MenuBar\PendingCreateMenuBar create()
+ * @method static void show()
+ * @method static void hide()
+ * @method static void label(string $label)
+ */
 class MenuBar extends Facade
 {
     protected static function getFacadeAccessor()
