8a0d4a38a4147e5c3b8c8a052b3c8617f0b0f222
YishOwlTing <mombuartworks@gmail.com>
2023-07-22 10:52:07 +0800

    Update Notification.php

--- src/Facades/Notification.php
+++ src/Facades/Notification.php
@@ -3,7 +3,15 @@
 namespace Native\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
+use phpDocumentor\Reflection\Types\Object_;
 
+/**
+ * @method static object new()
+ * @method static object title(string $title)
+ * @method static object event(string $event)
+ * @method static object message(string $body)
+ * @method static void show()
+ */
 class Notification extends Facade
 {
     protected static function getFacadeAccessor()
