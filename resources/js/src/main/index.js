82824f646005c114f830c92fcfa6474d90efcd44
Christopher Reed <chris@reedtech.us>
2023-07-22 00:00:03 -0500

    Temporarily changed php to php.exe in index for win platform

--- resources/js/src/main/index.js
+++ resources/js/src/main/index.js
@@ -3,7 +3,8 @@ import NativePHP from '@nativephp/electron-plugin'
 import {app} from 'electron'
 import certificate from '../../resources/cacert.pem?asset&asarUnpack'
 import defaultIcon from '../../resources/icon.png?asset&asarUnpack'
-import phpBinary from '../../resources/php?asset&asarUnpack'
+// TODO - Make this platform aware
+import phpBinary from '../../resources/php.exe?asset&asarUnpack'
 
 /**
  * Turn on the lights for the NativePHP app.
