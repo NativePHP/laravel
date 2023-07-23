d7b15c9df21627c8bc479c8264ce99bd063805a5
Christopher Reed <chris@reedtech.us>
2023-07-23 05:02:35 -0500

    Attempting new import method.

--- resources/js/src/main/index.js
+++ resources/js/src/main/index.js
@@ -1,13 +1,17 @@
 // import NativePHP from 'nativephp-electron'
+
 import NativePHP from '@nativephp/electron-plugin'
 import {app} from 'electron'
 import certificate from '../../resources/cacert.pem?asset&asarUnpack'
 import defaultIcon from '../../resources/icon.png?asset&asarUnpack'
-// TODO - Make this platform aware
 
-import phpBinary from ((process.platform === 'win32') 
-	? '../../resources/php/php.exe?asset&asarUnpack'
-	: '../../resources/php/php?asset&asarUnpack')
+// Probably still need to improve this platform awareness
+let phpBinary;
+if (process.platform === 'win32') {
+	phpBinary = import('../../resources/php/php.exe?asset&asarUnpack');
+} else {
+	phpBinary = import('../../resources/php/php?asset&asarUnpack');
+}
 
 /**
  * Turn on the lights for the NativePHP app.
