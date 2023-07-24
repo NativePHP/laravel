c1317e344162adf01c908bcbcf4a9f829cd7bd55
Christopher Reed <chris@reedtech.us>
2023-07-23 21:37:35 -0500

    Fixed formatting and removed older code

--- resources/js/src/main/index.js
+++ resources/js/src/main/index.js
@@ -1,5 +1,3 @@
-// import NativePHP from 'nativephp-electron'
-
 import NativePHP from '@nativephp/electron-plugin'
 import {app} from 'electron'
 
@@ -7,14 +5,6 @@ import NativePHP from '@nativephp/electron-plugin'
 import defaultIcon from '../../resources/icon.png?asset&asarUnpack'
 import phpBinary from '../../resources/php?asset&asarUnpack'
 import certificate from '../../resources/cacert.pem?asset&asarUnpack'
-
-// Probably still need to improve this platform awareness
-// let phpBinary;
-// if (process.platform === 'win32') {
-// 	phpBinary = import('../../resources/php/php.exe?asset&asarUnpack');
-// } else {
-// 	phpBinary = import('../../resources/php/php?asset&asarUnpack');
-// }
 
 /**
  * Turn on the lights for the NativePHP app.
