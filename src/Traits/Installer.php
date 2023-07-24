5fc9a837816a6b7413be9958016eff512106b960
Sergio Morales <sergiogmr@icloud.com>
2023-07-24 16:39:29 +0100

    The --installer option is not required in the $signature

--- src/Traits/Installer.php
+++ src/Traits/Installer.php
@@ -4,40 +4,45 @@ namespace Native\Electron\Traits;
 
 trait Installer
 {
-    protected function installDependencies()
+    protected function installNPMDependencies()
     {
         if ($this->option('force') || $this->confirm('Would you like to install the NativePHP NPM dependencies?', true)) {
             $this->comment('Installing NPM dependencies (This may take a while)...');
 
+            if (!this->option('installer')) {
+                $this->info('Installing NPM dependencies using the npm package manager...');
+                $this->installWithNPM();
+            }
+
             switch ($this->option('installer')) {
                 case 'yarn':
                     $this->info('Installing NPM dependencies using the yarn package manager...');
-                    $this->installYarnDependencies();
+                    $this->installWithYarn();
                     break;
                 case 'pnpm':
                     $this->info('Installing NPM dependencies using the pnpm package manager...');
-                    $this->installPnpmDependencies();
+                    $this->installWithPNPM();
                     break;
                 default:
                     $this->info('Installing NPM dependencies using the npm package manager...');
-                    $this->installNpmDependencies();
+                    $this->installWithNPM();
             }
 
             $this->output->newLine();
         }
     }
 
-    protected function installNpmDependencies()
+    protected function installWithNPM()
     {
         $this->executeCommand('npm set progress=false && npm install', $this->nativePhpPath());
     }
 
-    protected function installYarnDependencies()
+    protected function installWithYarn()
     {
         $this->executeCommand('yarn install', $this->nativePhpPath());
     }
 
-    protected function installPnpmDependencies()
+    protected function installWithPNPM()
     {
         $this->executeCommand('pnpm install', $this->nativePhpPath());
     }
