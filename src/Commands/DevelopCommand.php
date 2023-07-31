d9750fdef04788a854bc6a5f7331f10254feacf9
SergioGMR <sergiogmr@icloud.com>
2023-07-31 17:16:02 +0100

    Migrated the DevelopCommand to use --installer

--- src/Commands/DevelopCommand.php
+++ src/Commands/DevelopCommand.php
@@ -4,10 +4,15 @@ namespace Native\Electron\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Process;
+use Native\Electron\Concerns\LocatesPhpBinary;
+use Native\Electron\Traits\Developer;
+use Native\Electron\Traits\Installer;
 
 class DevelopCommand extends Command
 {
-    protected $signature = 'native:serve {--no-queue} {--D|no-dependencies}';
+    use Installer, Developer;
+
+    protected $signature = 'native:serve {--no-queue} {--D|no-dependencies} {--installer=npm}';
 
     public function handle()
     {
@@ -15,18 +20,12 @@ class DevelopCommand extends Command
 
         $this->info('Fetching latest dependencies…');
 
-        if (! $this->option('no-dependencies')) {
-            Process::path(__DIR__.'/../../resources/js/')
-                ->env([
-                    'NATIVEPHP_PHP_BINARY_PATH' => base_path('vendor/nativephp/php-bin/bin/mac'),
-                    'NATIVEPHP_CERTIFICATE_FILE_PATH' => base_path('vendor/nativephp/php-bin/cacert.pem'),
-                ])
-                ->forever()
-                ->run('yarn', function (string $type, string $output) {
-                    if ($this->getOutput()->isVerbose()) {
-                        echo $output;
-                    }
-                });
+        if (!$this->option('no-dependencies')) {
+            $this->installNPMDependencies(
+                force: !$this->option('no-dependencies'),
+                installer: $this->option('installer'
+                )
+            );
         }
 
         $this->info('Starting NativePHP app…');
@@ -35,20 +34,8 @@ class DevelopCommand extends Command
             $this->patchPlist();
         }
 
-        Process::path(__DIR__.'/../../resources/js/')
-            ->env([
-                'APP_PATH' => base_path(),
-                'NATIVEPHP_PHP_BINARY_PATH' => base_path('vendor/nativephp/php-bin/bin/mac'),
-                'NATIVEPHP_CERTIFICATE_FILE_PATH' => base_path('vendor/nativephp/php-bin/cacert.pem'),
-                'NATIVE_PHP_SKIP_QUEUE' => $this->option('no-queue') ? true : false,
-            ])
-            ->forever()
-            ->tty(PHP_OS_FAMILY != 'Windows')
-            ->run('yarn run dev', function (string $type, string $output) {
-                if ($this->getOutput()->isVerbose()) {
-                    echo $output;
-                }
-            });
+        $this->runDeveloper(installer: $this->option('installer'), skip_queue: $this->option('no-queue'));
+
     }
 
     /**
@@ -59,15 +46,15 @@ class DevelopCommand extends Command
      */
     protected function patchPlist()
     {
-        $pList = file_get_contents(__DIR__.'/../../resources/js/node_modules/electron/dist/Electron.app/Contents/Info.plist');
+        $pList = file_get_contents(__DIR__ . '/../../resources/js/node_modules/electron/dist/Electron.app/Contents/Info.plist');
 
         // Change the CFBundleName to the correct app name
         $pattern = '/(<key>CFBundleName<\/key>\s+<string>)(.*?)(<\/string>)/m';
-        $pList = preg_replace($pattern, '$1'.config('app.name').'$3', $pList);
+        $pList = preg_replace($pattern, '$1' . config('app.name') . '$3', $pList);
 
         $pattern = '/(<key>CFBundleDisplayName<\/key>\s+<string>)(.*?)(<\/string>)/m';
-        $pList = preg_replace($pattern, '$1'.config('app.name').'$3', $pList);
+        $pList = preg_replace($pattern, '$1' . config('app.name') . '$3', $pList);
 
-        file_put_contents(__DIR__.'/../../resources/js/node_modules/electron/dist/Electron.app/Contents/Info.plist', $pList);
+        file_put_contents(__DIR__ . '/../../resources/js/node_modules/electron/dist/Electron.app/Contents/Info.plist', $pList);
     }
 }
---
+++ src/Traits/Developer.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Native\Electron\Traits;
+
+use Native\Electron\Concerns\LocatesPhpBinary;
+
+trait Developer
+{
+    use ExecuteCommand;
+
+    protected function runDeveloper(string $installer, bool $skip_queue): void
+    {
+        [$installer, $command] = $this->getInstallerAndCommand(installer: $installer, type: 'dev');
+
+        $this->info("Runing the dev script with {$installer}...");
+        $this->executeCommand(command: $command, type: 'serve', skip_queue: $skip_queue);
+    }
+
+}
