f3c727d4bdd43f60e585acce3b6216a5b1042b8d
miladev-ent <98118400+milwad-dev@users.noreply.github.com>
2023-08-01 01:54:54 +0330

    wip

--- tests/Windows/WindowTest.php
+++ tests/Windows/WindowTest.php
@@ -5,6 +5,19 @@ it('test window', function () {
         ->id('main')
         ->title('milwad')
         ->titleBarStyle('milwad')
+        ->rememberState()
+        ->frameless()
+        ->focusable()
+        ->hasShadow()
+        ->alwaysOnTop()
+        ->showDevTools()
+        ->resizable()
+        ->movable()
+        ->minimizable()
+        ->maximizable()
+        ->closable()
+        ->fullscreen()
+        ->kiosk()
         ->hideMenu();
 
     $windowArray = $window->toArray();
@@ -12,5 +25,18 @@ it('test window', function () {
     expect($windowArray['id'])->toBe('main');
     expect($windowArray['title'])->toBe('milwad');
     expect($windowArray['titleBarStyle'])->toBe('milwad');
+    expect($windowArray['rememberState'])->toBeTrue();
+    expect($windowArray['frame'])->toBeFalse();
+    expect($windowArray['focusable'])->toBeTrue();
+    expect($windowArray['hasShadow'])->toBeTrue();
+    expect($windowArray['alwaysOnTop'])->toBeTrue();
+    expect($windowArray['showDevTools'])->toBeTrue();
+    expect($windowArray['resizable'])->toBeTrue();
+    expect($windowArray['movable'])->toBeTrue();
+    expect($windowArray['minimizable'])->toBeTrue();
+    expect($windowArray['maximizable'])->toBeTrue();
+    expect($windowArray['closable'])->toBeTrue();
+    expect($windowArray['fullscreen'])->toBeFalse();
+    expect($windowArray['kiosk'])->toBeFalse();
     expect($windowArray['autoHideMenuBar'])->toBeTrue();
 });
