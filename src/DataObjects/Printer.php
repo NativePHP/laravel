84bad1626993fc1dc1e7959c87dc593df9269afd
Marcel Pociot <m.pociot@gmail.com>
2023-08-01 11:31:14 +0200

    Add print support

---
+++ src/DataObjects/Printer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Native\Laravel\DataObjects;
+
+class Printer
+{
+    public function __construct(
+        public string $name,
+        public string $displayName,
+        public string $description,
+        public int $status,
+        public bool $isDefault,
+        public array $options
+    )
+    {
+
+    }
+}
