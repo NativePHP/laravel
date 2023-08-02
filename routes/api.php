ae1266e2daad7834d47931b0d9ad0064c3d340a8
Hossain Mohammad Shahidullah Jaber <mdsjaber@gmail.com>
2023-08-03 00:48:38 +0600

    import VerifyCsrfToken

--- routes/api.php
+++ routes/api.php
@@ -1,14 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Native\Laravel\Http\Controllers\CreateSecurityCookieController;
-use Native\Laravel\Http\Controllers\DispatchEventFromAppController;
+use App\Http\Middleware\VerifyCsrfToken;
 use Native\Laravel\Http\Controllers\NativeAppBootedController;
 use Native\Laravel\Http\Middleware\PreventRegularBrowserAccess;
+use Native\Laravel\Http\Controllers\CreateSecurityCookieController;
+use Native\Laravel\Http\Controllers\DispatchEventFromAppController;
 
 Route::group(['middleware' => PreventRegularBrowserAccess::class], function () {
     Route::post('_native/api/booted', NativeAppBootedController::class);
     Route::post('_native/api/events', DispatchEventFromAppController::class);
-})->withoutMiddleware(\App\Http\Middleware\VerifyCsrfToken::class);
+})->withoutMiddleware(VerifyCsrfToken::class);
 
 Route::get('_native/api/cookie', CreateSecurityCookieController::class);
