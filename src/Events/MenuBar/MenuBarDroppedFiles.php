9971e81e2e683488848b532df7b0963623ad8f03
Aron Rotteveel <hi@aron.codes>
2023-08-03 00:41:25 +0200

    Add files to the event

--- src/Events/MenuBar/MenuBarDroppedFiles.php
+++ src/Events/MenuBar/MenuBarDroppedFiles.php
@@ -12,6 +12,10 @@ class MenuBarDroppedFiles implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
+    public function __construct(public array $files = [])
+    {
+    }
+
     public function broadcastOn()
     {
         return [
