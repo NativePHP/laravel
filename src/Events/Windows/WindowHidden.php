ef82ca3865b83703fe763dc1c90b07665cc9479d
Ethan Brace <ethanabrace@gmail.com>
2023-08-03 11:31:31 -0400

    add hide method + event

---
+++ src/Events/Windows/WindowHidden.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Native\Laravel\Events\Windows;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class WindowHidden implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct(public string $id)
+    {
+        //
+    }
+
+    public function broadcastOn()
+    {
+        return [
+            new Channel('nativephp'),
+        ];
+    }
+}
