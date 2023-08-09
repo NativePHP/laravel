57e18b765dc6d652142d1592c9f892bfd45a4840
Dobren Dragojević <100283771+ddobren@users.noreply.github.com>
2023-08-09 12:46:39 +0200

    Update LoadStartupConfigurationCommand.php
    Fix styling

--- src/Commands/LoadStartupConfigurationCommand.php
+++ src/Commands/LoadStartupConfigurationCommand.php
@@ -6,10 +6,21 @@ use Illuminate\Console\Command;
 
 class LoadStartupConfigurationCommand extends Command
 {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
     protected $signature = 'native:config';
 
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
     public function handle()
     {
         echo json_encode(config('nativephp'));
+        return 0;
     }
 }
