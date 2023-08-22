99386bf583057fd22e483d1173fcf5e14e4c2c35
Dan Johnson <dan@try.be>
2023-08-22 21:19:45 +0100

    feat: adds the required config for using GitHub as an updater provider

--- config/nativephp.php
+++ config/nativephp.php
@@ -66,11 +66,22 @@ return [
 
         /**
          * The updater provider to use.
-         * Supported: "s3", "spaces"
+         * Supported: "github", "s3", "spaces"
          */
         'default' => env('NATIVEPHP_UPDATER_PROVIDER', 'spaces'),
 
         'providers' => [
+            'github' => [
+                'driver' => 'github',
+                'repo' => env('GITHUB_REPO'),
+                'owner' => env('GITHUB_OWNER'),
+                'token' => env('GITHUB_TOKEN'),
+                'vPrefixedTagName' => env('GITHUB_V_PREFIXED_TAG_NAME', true),
+                'private' => env('GITHUB_PRIVATE', false),
+                'channel' => env('GITHUB_CHANNEL', 'latest'),
+                'releaseType' => env('GITHUB_RELEASE_TYPE', 'draft'),
+            ],
+
             's3' => [
                 'driver' => 's3',
                 'key' => env('AWS_ACCESS_KEY_ID'),
