1c2157ac2e00d255e8b952ca31634c3e73b33586
Milwad <milwad.dev@gmail.com>
2024-04-02 13:56:56 +0330

    add missing method to System Facade

--- src/Facades/System.php
+++ src/Facades/System.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static bool canPromptTouchID()
  * @method static bool promptTouchID(string $reason)
+ * @method static array printers()
+ * @method static void print(string $html, ?\Native\Laravel\DataObjects\Printer $printer = null)
+ * @method static string printToPDF(string $html)
  */
 class System extends Facade
 {
