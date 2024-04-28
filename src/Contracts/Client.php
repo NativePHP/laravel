43cf1921a0016985e0774c75e9bbb1b4ae1abd96
simonhamp <simonhamp@users.noreply.github.com>
2024-04-28 16:38:37 +0000

    Fix styling

--- src/Contracts/Client.php
+++ src/Contracts/Client.php
@@ -5,6 +5,8 @@ namespace Native\Laravel\Contracts;
 interface Client
 {
     public function get(string $resource);
+
     public function post(string $resource, array $data = []);
+
     public function delete(string $resource, array $data = []);
 }
