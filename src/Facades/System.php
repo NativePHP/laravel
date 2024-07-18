047c774013fe3c1fad9b9b04870bb570973467c2
Simon Hamp <simon.hamp@me.com>
2024-07-18 05:07:20 +0100

    docblocks

--- src/Facades/System.php
+++ src/Facades/System.php
@@ -7,6 +7,10 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static bool canPromptTouchID()
  * @method static bool promptTouchID(string $reason)
+ * @method static array printers()
+ * @method static void print(string $html, ?\Native\Laravel\DataObjects\Printer $printer = null)
+ * @method static string printToPDF(string $reason)
+ * @method static string timezone()
  */
 class System extends Facade
 {
