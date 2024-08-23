7397d15e3ddc2d14c84ea096147f6140bc4473ba
Robert Wesner <robert@wesner.io>
2024-08-24 00:38:03 +0200

    Implemented minimizing (#347)

--- src/Facades/Window.php
+++ src/Facades/Window.php
@@ -12,6 +12,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static void position($x, $y, $animated = false, $id = null)
  * @method static void alwaysOnTop($alwaysOnTop = true, $id = null)
  * @method static void reload($id = null)
+ * @method static void maximize($id = null)
+ * @method static void minimize($id = null)
  */
 class Window extends Facade
 {
