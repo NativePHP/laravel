949a06e73c0e9f7cb49dee78ae6f544f6b2bd380
Simon Hamp <simon.hamp@me.com>
2024-09-08 15:34:30 +0100

    Add docblock to facade
    Helps with IDE support

--- src/Facades/Shell.php
+++ src/Facades/Shell.php
@@ -4,6 +4,12 @@ namespace Native\Laravel\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static void showInFolder(string $path)
+ * @method static string openFile(string $path)
+ * @method static void trashFile(string $path)
+ * @method static void openExternal(string $url)
+ */
 class Shell extends Facade
 {
     protected static function getFacadeAccessor()
