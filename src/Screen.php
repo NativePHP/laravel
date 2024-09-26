d746bfb344455bba15c28447b0a1d3db77b269fa
Andrei Daniel Petrica <andreidaniel.petrica@dedagroup.it>
2024-09-26 23:59:50 +0200

    added getCenterOfActiveScreen in Screen class
    Added function that Returns the center of the screen where the mouse pointer is placed. Useful if you want to initialise a smaller window and place it exactly in the center of screen.

--- src/Screen.php
+++ src/Screen.php
@@ -17,4 +17,40 @@ class Screen
     {
         return $this->client->get('screen/displays')->json('displays');
     }
+
+    /**
+     *  Returns the center of the screen where the mouse pointer is placed.
+     *
+     * @return array<string,int>
+     */
+    public function getCenterOfActiveScreen(): array
+    {
+        //get the screen size and current cursor position.
+        $cursor = $this->cursorPosition();
+        $screen = $this->displays();
+
+        /* Navigate every screen and check for cursor position against the bounds of the screen. */
+        foreach ($screen as $s) {
+            /** @var array $bounds  */
+            $bounds = $s['bounds'];
+            if (
+                $cursor->x >= $bounds['x']
+                && $cursor->x <= ($bounds['x'] + $bounds['width'])
+                && $cursor->y >= $bounds['y']
+                && $cursor->y <= ($bounds['y'] + $bounds['height'])
+            ) {
+                // I'm inside the display
+                return [
+                    'x' => $bounds['x'] + $bounds['width'] / 2,
+                    'y' => $bounds['y'] + $bounds['height'] / 2,
+                ];
+            }
+        }
+        $bounds = $screen[0]['bounds'];
+
+        return [
+            'x' => $bounds['x'] + $bounds['width'] / 2,
+            'y' => $bounds['y'] + $bounds['height'] / 2,
+        ];
+    }
 }
