405dc511da9bc0441e252d87ac46db47fdb6aca4
Andrei Daniel Petrica <andreidaniel.petrica@dedagroup.it>
2024-10-19 18:22:13 +0200

    added methods, cleaned imports, restored 4 spaces indent and removed useless method.

--- src/Facades/Screen.php
+++ src/Facades/Screen.php
@@ -8,6 +8,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static object cursorPosition()
  * @method static array displays()
  * @method static array getCenterOfActiveScreen()
+ * @method static array active()
+ * @method static array primary()
  */
 class Screen extends Facade
 {
--- src/Screen.php
+++ src/Screen.php
@@ -3,54 +3,48 @@
 namespace Native\Laravel;
 
 use Native\Laravel\Client\Client;
-use Illuminate\Support\Facades\Log;
 
 class Screen
 {
-  public function __construct(protected Client $client)
-  {
-  }
+    public function __construct(protected Client $client)
+    {
+    }
 
-  public function getClient()
-  {
-    return $this->client;
-  }
+    public function cursorPosition(): object
+    {
+        return (object) $this->client->get("screen/cursor-position")->json();
+    }
 
-  public function cursorPosition(): object
-  {
-    return (object) $this->client->get("screen/cursor-position")->json();
-  }
+    public function displays(): array
+    {
+        return $this->client->get("screen/displays")->json("displays");
+    }
 
-  public function displays(): array
-  {
-    return $this->client->get("screen/displays")->json("displays");
-  }
+    public function primary(): object
+    {
+        return $this->client->get("screen/primary-display")->json("primaryDisplay");
+    }
 
-  public function primary(): object
-  {
-    return $this->client->get("screen/primary-display")->json("primaryDisplay");
-  }
+    public function active(): object
+    {
+        return $this->client->get("screen/active")->json();
+    }
 
-  public function active(): object
-  {
-    return $this->client->get("screen/active")->json();
-  }
+    /**
+     *  Returns the center of the screen where the mouse pointer is placed.
+     *
+     * @return array<string,int>
+     */
+    public function getCenterOfActiveScreen(): array
+    {
+        /* Navigate every screen and check for cursor position against the bounds of the screen. */
+        $activeScreen = $this->active();
 
-  /**
-   *  Returns the center of the screen where the mouse pointer is placed.
-   *
-   * @return array<string,int>
-   */
-  public function getCenterOfActiveScreen(): array
-  {
-    /* Navigate every screen and check for cursor position against the bounds of the screen. */
-    $activeScreen = $this->active();
+        $bounds = $activeScreen["bounds"];
 
-    $bounds = $activeScreen["bounds"];
-
-    return [
-      "x" => $bounds["x"] + $bounds["width"] / 2,
-      "y" => $bounds["y"] + $bounds["height"] / 2,
-    ];
-  }
+        return [
+            "x" => $bounds["x"] + $bounds["width"] / 2,
+            "y" => $bounds["y"] + $bounds["height"] / 2,
+        ];
+    }
 }
