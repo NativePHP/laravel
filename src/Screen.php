7839fcf53b11792d7fb5479754bd17e6a86988f6
Andrei Daniel Petrica <andreidaniel.petrica@dedagroup.it>
2024-10-19 14:38:16 +0200

    cleaned and finsihed

--- src/Screen.php
+++ src/Screen.php
@@ -43,37 +43,10 @@ class Screen
    */
   public function getCenterOfActiveScreen(): array
   {
-    //get the screen size and current cursor position.
-    $cursor = $this->cursorPosition();
-    $screen = $this->displays();
-
     /* Navigate every screen and check for cursor position against the bounds of the screen. */
     $activeScreen = $this->active();
-    $resp = $this->client->get("screen/active");
-    Log::debug("activeScreen", [
-      $activeScreen,
-      $resp->body(),
-      $resp->headers(),
-      $resp->status(),
-    ]);
 
-    foreach ($screen as $s) {
-      /** @var array $bounds  */
-      $bounds = $s["bounds"];
-      if (
-        $cursor->x >= $bounds["x"] &&
-        $cursor->x <= $bounds["x"] + $bounds["width"] &&
-        $cursor->y >= $bounds["y"] &&
-        $cursor->y <= $bounds["y"] + $bounds["height"]
-      ) {
-        // I'm inside the display
-        return [
-          "x" => $bounds["x"] + $bounds["width"] / 2,
-          "y" => $bounds["y"] + $bounds["height"] / 2,
-        ];
-      }
-    }
-    $bounds = $screen[0]["bounds"];
+    $bounds = $activeScreen["bounds"];
 
     return [
       "x" => $bounds["x"] + $bounds["width"] / 2,
