1176d51af35e2dd4bfe0a232ab9403a0e99740d4
Simon Hamp <simon.hamp@me.com>
2024-10-20 16:08:02 +0100

    wip

---
+++ src/ChildProcess/ChildProcess.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace Native\Laravel\ChildProcess;
+
+use Illuminate\Support\Facades\Event;
+use Native\Laravel\Client\Client;
+use Native\Laravel\Events\ChildProcess\ErrorReceived;
+use Native\Laravel\Events\ChildProcess\MessageReceived;
+use Native\Laravel\Events\ChildProcess\ProcessSpawned;
+use Native\Laravel\Events\ChildProcess\ProcessExited;
+
+class ChildProcess
+{
+    private string $alias;
+    private ?array $process;
+
+    public function __construct(protected Client $client) {}
+
+    public function start(string $alias, array $cmd, string $cwd = null, array $env = null): object
+    {
+        $this->alias = $alias;
+
+        $this->process = $this->client->post('child-process/start', [
+            'alias' => $alias,
+            'cmd' => $cmd,
+            'cwd' => base_path(),
+            'env' => $env,
+        ])->json();
+
+        return $this;
+    }
+
+    public function stop(string $alias): void
+    {
+        $this->client->post('child-process/stop', [
+            'alias' => $alias,
+        ])->json();
+    }
+
+    public function message(string $alias, mixed $message): void
+    {
+        $this->client->post('child-process/message', [
+            'alias' => $alias,
+            'message' => json_encode($message),
+        ])->json();
+    }
+
+    public function onMessage(\Closure $callback) {
+        // Event::listen(function (MessageReceived $event) use ($callback) {
+        //    if ($event->alias !== $this->alias) {
+        //        return;
+        //    }
+        //
+        //    $callback($event);
+        // );
+        return $this;
+    }
+
+    public function onError(\Closure $callback) {
+        // Event::listen(function (ErrorReceived $event) use ($callback) {
+        //    if ($event->alias !== $this->alias) {
+        //        return;
+        //    }
+        //
+        //    $callback($event);
+        // );
+        return $this;
+    }
+
+    public function onSpawn(\Closure $callback) {
+        // Event::listen(function (ProcessSpawned $event) use ($callback) {
+        //    if ($event->alias !== $this->alias) {
+        //        return;
+        //    }
+        //
+        //    $callback($event);
+        // );
+        return $this;
+    }
+
+    public function onExit(\Closure $callback) {
+        // Event::listen(function (ProcessExited $event) use ($callback) {
+        //    if ($event->alias !== $this->alias) {
+        //        return;
+        //    }
+        //
+        //    $callback($event);
+        // );
+        return $this;
+    }
+}
---
+++ src/ChildProcess/ChildProcessManager.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Native\Laravel\ChildProcess;
+
+use Native\Laravel\Client\Client;
+
+class ChildProcessManager
+{
+    public function __construct(protected Client $client) {}
+
+    public function start(string $alias, array $cmd, string $cwd = null, array $env = null): object
+    {
+        $this->process = $this->client->post('child-process/start')->json([
+            'alias' => $alias,
+            'cmd' => $cmd,
+            'cwd' => $cwd,
+            'env' => $env,
+        ]);
+    }
+
+    public function onMessage() {
+
+    }
+}
