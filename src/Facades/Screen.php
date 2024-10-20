b5982c5c834af508950f5a23a0668e728b8c6f16
Daniel, Petrica Andrei-Daniel <andreidaniel.petrica@dedagroup.it>
2024-10-20 18:12:28 +0200

    added getCenterOfActiveScreen in Screen class (#375)

--- src/Facades/Screen.php
+++ src/Facades/Screen.php
@@ -7,6 +7,9 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static object cursorPosition()
  * @method static array displays()
+ * @method static array getCenterOfActiveScreen()
+ * @method static array active()
+ * @method static array primary()
  */
 class Screen extends Facade
 {
--- src/Screen.php
+++ src/Screen.php
@@ -6,7 +6,9 @@ use Native\Laravel\Client\Client;
 
 class Screen
 {
-    public function __construct(protected Client $client) {}
+    public function __construct(protected Client $client)
+    {
+    }
 
     public function cursorPosition(): object
     {
@@ -16,5 +18,33 @@ class Screen
     public function displays(): array
     {
         return $this->client->get('screen/displays')->json('displays');
+    }
+
+    public function primary(): object
+    {
+        return $this->client->get('screen/primary-display')->json('primaryDisplay');
+    }
+
+    public function active(): object
+    {
+        return $this->client->get('screen/active')->json();
+    }
+
+    /**
+     *  Returns the center of the screen where the mouse pointer is placed.
+     *
+     * @return array<string,int>
+     */
+    public function getCenterOfActiveScreen(): array
+    {
+        /* Navigate every screen and check for cursor position against the bounds of the screen. */
+        $activeScreen = $this->active();
+
+        $bounds = $activeScreen['bounds'];
+
+        return [
+            'x' => $bounds['x'] + $bounds['width'] / 2,
+            'y' => $bounds['y'] + $bounds['height'] / 2,
+        ];
     }
 }
