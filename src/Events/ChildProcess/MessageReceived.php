a99d2724ea24707868313973e8c6f9585e26b885
Simon Hamp <simon.hamp@me.com>
2024-10-24 20:37:08 +0100

    add events

---
+++ src/Events/ChildProcess/MessageReceived.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Native\Laravel\Events\ChildProcess;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MessageReceived implements ShouldBroadcast
+{
+    use Dispatchable, SerializesModels;
+
+    public function __construct(public string $alias, public mixed $data) {}
+
+    public function broadcastOn()
+    {
+        return [
+            new Channel('nativephp'),
+        ];
+    }
+}
