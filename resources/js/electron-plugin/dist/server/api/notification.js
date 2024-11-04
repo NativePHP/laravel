74ed980b986e03dbf46b8752905d08191777124f
Simon Hamp <simon.hamp@me.com>
2024-11-04 11:17:10 +0000

    Fix + code style

--- resources/js/electron-plugin/dist/server/api/notification.js
+++ resources/js/electron-plugin/dist/server/api/notification.js
@@ -3,9 +3,23 @@ import { Notification } from 'electron';
 import { notifyLaravel } from "../utils";
 const router = express.Router();
 router.post('/', (req, res) => {
-    const { title, body, subtitle, silent, icon, hasReply, timeoutType, replyPlaceholder, sound, urgency, actions, closeButtonText, toastXml, customEvent: event } = req.body;
+    const { title, body, subtitle, silent, icon, hasReply, timeoutType, replyPlaceholder, sound, urgency, actions, closeButtonText, toastXml, event: customEvent } = req.body;
     const eventName = customEvent !== null && customEvent !== void 0 ? customEvent : '\\Native\\Laravel\\Events\\Notifications\\NotificationClicked';
-    const notification = new Notification({ title, body, subtitle, silent, icon, hasReply, timeoutType, replyPlaceholder, sound, urgency, actions, closeButtonText, toastXml });
+    const notification = new Notification({
+        title,
+        body,
+        subtitle,
+        silent,
+        icon,
+        hasReply,
+        timeoutType,
+        replyPlaceholder,
+        sound,
+        urgency,
+        actions,
+        closeButtonText,
+        toastXml
+    });
     notification.on("click", (event) => {
         notifyLaravel('events', {
             event: eventName,
--- resources/js/electron-plugin/src/server/api/notification.ts
+++ resources/js/electron-plugin/src/server/api/notification.ts
@@ -1,24 +1,54 @@
-import express from 'express'
-import { Notification } from 'electron'
+import express from 'express';
+import { Notification } from 'electron';
 import {notifyLaravel} from "../utils";
 const router = express.Router();
 
 router.post('/', (req, res) => {
-    const {title, body, subtitle, silent, icon, hasReply, timeoutType, replyPlaceholder, sound, urgency, actions, closeButtonText, toastXml, customEvent: event} = req.body
+    const {
+        title,
+        body,
+        subtitle,
+        silent,
+        icon,
+        hasReply,
+        timeoutType,
+        replyPlaceholder,
+        sound,
+        urgency,
+        actions,
+        closeButtonText,
+        toastXml,
+        event: customEvent
+    } = req.body;
+
     const eventName = customEvent ?? '\\Native\\Laravel\\Events\\Notifications\\NotificationClicked';
 
-    const notification = new Notification({title, body, subtitle, silent, icon, hasReply, timeoutType, replyPlaceholder, sound, urgency, actions, closeButtonText, toastXml});
+    const notification = new Notification({
+        title,
+        body,
+        subtitle,
+        silent,
+        icon,
+        hasReply,
+        timeoutType,
+        replyPlaceholder,
+        sound,
+        urgency,
+        actions,
+        closeButtonText,
+        toastXml
+    });
 
-    notification.on("click", (event)=>{
+    notification.on("click", (event) => {
         notifyLaravel('events', {
             event: eventName,
             payload: JSON.stringify(event)
-        })
-    })
+        });
+    });
 
-    notification.show()
+    notification.show();
 
-    res.sendStatus(200)
+    res.sendStatus(200);
 });
 
 export default router;
