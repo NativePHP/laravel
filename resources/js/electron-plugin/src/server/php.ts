502bfe5a2aebe2599fb0bceda06b827150c9e786
Jerke Combee <JCombee@users.noreply.github.com>
2024-11-10 22:20:49 +0100

    [BUG] Change the relative path to the application in order to make it line up again (#125)

--- resources/js/electron-plugin/src/server/php.ts
+++ resources/js/electron-plugin/src/server/php.ts
@@ -96,7 +96,7 @@ function getArgumentEnv() {
 }
 
 function getAppPath() {
-    let appPath = join(__dirname, '../../../../../resources/app/').replace('app.asar', 'app.asar.unpacked')
+    let appPath = join(__dirname, '../../resources/app/').replace('app.asar', 'app.asar.unpacked')
 
     if (process.env.NODE_ENV === 'development' || argumentEnv.TESTING == 1) {
         appPath = process.env.APP_PATH || argumentEnv.APP_PATH;
