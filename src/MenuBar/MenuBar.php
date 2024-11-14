c223e0f3e630260bf3e522002ac987c64c193b1c
Simon Hamp <simon.hamp@me.com>
2024-11-14 18:13:17 +0000

    Feature/menubar improvement (#411)
    * Refactor variable
    
    * Remove override method
    
    `url` is already available from the `HasUrl` trait
    
    * Add tooltip setting
    
    * Add resizable setting
    
    * Allow updating of tooltip
    
    * Allow updating of icon
    
    * Support firing a custom event on click

--- src/MenuBar/MenuBar.php
+++ src/MenuBar/MenuBar.php
@@ -20,11 +20,17 @@ class MenuBar
 
     protected string $label = '';
 
-    protected bool $onlyShowContextWindow = false;
+    protected string $tooltip = '';
+
+    protected bool $resizable = true;
+
+    protected bool $onlyShowContextMenu = false;
 
     protected ?Menu $contextMenu = null;
 
     protected bool $alwaysOnTop = false;
+
+    protected ?string $event = null;
 
     protected bool $showDockIcon = false;
 
@@ -51,14 +57,7 @@ class MenuBar
 
     public function onlyShowContextMenu(bool $onlyContextMenu = true): self
     {
-        $this->onlyShowContextWindow = $onlyContextMenu;
-
-        return $this;
-    }
-
-    public function url(string $url): self
-    {
-        $this->url = $url;
+        $this->onlyShowContextMenu = $onlyContextMenu;
 
         return $this;
     }
@@ -77,9 +76,30 @@ class MenuBar
         return $this;
     }
 
+    public function tooltip(string $tooltip = ''): self
+    {
+        $this->tooltip = $tooltip;
+
+        return $this;
+    }
+
+    public function resizable(bool $resizable = true): static
+    {
+        $this->resizable = $resizable;
+
+        return $this;
+    }
+
     public function alwaysOnTop($alwaysOnTop = true): self
     {
         $this->alwaysOnTop = $alwaysOnTop;
+
+        return $this;
+    }
+
+    public function event(string $event): self
+    {
+        $this->event = $event;
 
         return $this;
     }
@@ -100,15 +120,18 @@ class MenuBar
             'x' => $this->x,
             'y' => $this->y,
             'label' => $this->label,
+            'tooltip' => $this->tooltip,
+            'resizable' => $this->resizable,
             'width' => $this->width,
             'height' => $this->height,
             'vibrancy' => $this->vibrancy,
             'showDockIcon' => $this->showDockIcon,
             'transparency' => $this->transparent,
             'backgroundColor' => $this->backgroundColor,
-            'onlyShowContextWindow' => $this->onlyShowContextWindow,
+            'onlyShowContextMenu' => $this->onlyShowContextMenu,
             'contextMenu' => ! is_null($this->contextMenu) ? $this->contextMenu->toArray()['submenu'] : null,
             'alwaysOnTop' => $this->alwaysOnTop,
+            'event' => $this->event,
         ];
     }
 }
