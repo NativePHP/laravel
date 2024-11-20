23c33638a36b42cc31859a2faca7e9230789efec
Simon Hamp <simon.hamp@me.com>
2024-11-20 02:35:31 +0000

    Style

--- src/Menu/Items/Checkbox.php
+++ src/Menu/Items/Checkbox.php
@@ -6,8 +6,11 @@ class Checkbox extends MenuItem
 {
     protected string $type = 'checkbox';
 
-    public function __construct(string $label, protected bool $isChecked = false, protected ?string $accelerator = null)
-    {
+    public function __construct(
+        string $label,
+        protected bool $isChecked = false,
+        protected ?string $accelerator = null
+    ) {
         $this->label = $label;
     }
 }
