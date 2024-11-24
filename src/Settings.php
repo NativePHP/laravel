1f4721c55cef1d74273ce29e21404dade18f2373
Eser DENIZ <srwiez@gmail.com>
2024-11-24 18:42:49 +0100

    feature: improve Settings

--- src/Settings.php
+++ src/Settings.php
@@ -8,15 +8,25 @@ class Settings
 {
     public function __construct(protected Client $client) {}
 
-    public function set($key, $value): void
+    public function set(string $key, $value): void
     {
         $this->client->post('settings/'.$key, [
             'value' => $value,
         ]);
     }
 
-    public function get($key, $default = null): mixed
+    public function get(string $key, $default = null): mixed
     {
         return $this->client->get('settings/'.$key)->json('value') ?? $default;
+    }
+
+    public function forget(string $key): void
+    {
+        $this->client->delete('settings/'.$key);
+    }
+
+    public function clear(): void
+    {
+        $this->client->delete('settings/');
     }
 }
