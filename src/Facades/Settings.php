41459c2a9df696741febf960cfd46f669f89f636
Eser DENIZ <srwiez@gmail.com>
2024-11-25 14:56:20 +0100

    feature: improve Settings (#432)

--- src/Facades/Settings.php
+++ src/Facades/Settings.php
@@ -5,8 +5,10 @@ namespace Native\Laravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static void set($key, $value)
- * @method static mixed get($key, $default = null)
+ * @method static void set(string $key, $value)
+ * @method static mixed get(string $key, $default = null)
+ * @method static void forget(string $key)
+ * @method static void clear()
  */
 class Settings extends Facade
 {
