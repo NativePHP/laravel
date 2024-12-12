d6823632ca50a138a682782532b8c6aebd866261
Eser DENIZ <srwiez@gmail.com>
2024-12-12 09:27:23 +0100

    feat: additional events

---
+++ src/Events/PowerMonitor/Shutdown.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Native\Laravel\Events\PowerMonitor;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class Shutdown implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function __construct() {}
+
+    public function broadcastOn()
+    {
+        return [
+            new Channel('nativephp'),
+        ];
+    }
+}
