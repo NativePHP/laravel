9a4cb724b6b262f3dc6f857199604a496f226d60
Eser DENIZ <srwiez@gmail.com>
2024-12-12 09:30:48 +0100

    fix: PowerMonitor Facade docblock

--- src/Facades/PowerMonitor.php
+++ src/Facades/PowerMonitor.php
@@ -5,7 +5,7 @@ namespace Native\Laravel\Facades;
 use Illuminate\Support\Facades\Facade;
 
 /**
- * @method static \Native\Laravel\Enums\SystemIdelStatesEnum getSystemIdleState(int $threshold)
+ * @method static \Native\Laravel\Enums\SystemIdleStatesEnum getSystemIdleState(int $threshold)
  * @method static int getSystemIdleTime()
  * @method static \Native\Laravel\Enums\ThermalStatesEnum getCurrentThermalState()
  * @method static bool isOnBatteryPower()
