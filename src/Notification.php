46f3e19b89f5b6253c4953bc4deab09e9b3b3a95
Eser DENIZ <srwiez@gmail.com>
2024-12-25 21:57:40 +0100

    feat: default notification title

--- src/Notification.php
+++ src/Notification.php
@@ -12,7 +12,9 @@ class Notification
 
     protected string $event = '';
 
-    final public function __construct(protected Client $client) {}
+    final public function __construct(protected Client $client) {
+        $this->title = config('app.name');
+    }
 
     public static function new()
     {
