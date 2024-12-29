8d24601f1d659f36977b66b1969bfde6ab311db1
Eser DENIZ <srwiez@gmail.com>
2024-12-29 16:18:09 +0100

    feat: new menubar event

---
+++ src/Events/MenuBar/MenuBarCreated.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Native\Laravel\Events\MenuBar;
+
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Queue\SerializesModels;
+
+class MenuBarCreated implements ShouldBroadcastNow
+{
+    use Dispatchable, InteractsWithSockets, SerializesModels;
+
+    public function broadcastOn()
+    {
+        return [
+            new Channel('nativephp'),
+        ];
+    }
+}
