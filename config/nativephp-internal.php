45d0d424e3a0d0d2a6223b80b9e021a4467d7245
gwleuverink <willem@leuver.ink>
2025-01-16 17:36:41 +0100

    moved internal cleanup_exclude files back again
    This is hard to test when the config I'm asserting on lives on a different repo from the test itself.

--- config/nativephp-internal.php
+++ config/nativephp-internal.php
@@ -29,37 +29,4 @@ return [
      * The URL to the NativePHP API.
      */
     'api_url' => env('NATIVEPHP_API_URL', 'http://localhost:4000/api/'),
-
-    /**
-     * The default list of files to exclude from the build
-     */
-    'cleanup_exclude_files' => [
-        // .git and dev directories
-        '.git',
-        'dist',
-        'docker',
-        'packages',
-        '**/.github',
-
-        // Potentially containing sensitive info
-        'database/*.sqlite',
-        'database/*.sqlite-shm',
-        'database/*.sqlite-wal',
-
-        'storage/framework/sessions/*',
-        'storage/framework/testing/*',
-        'storage/framework/cache/*',
-        'storage/framework/views/*',
-        'storage/logs/*',
-
-        // Only needed for local testing
-        'vendor/nativephp/electron/resources',
-        'vendor/nativephp/electron/vendor',
-        'vendor/nativephp/electron/bin',
-        'vendor/nativephp/laravel/vendor',
-        'vendor/nativephp/php-bin',
-
-        // Also deleted in PrunesVendorDirectory after fresh composer install
-        'vendor/bin'
-    ]
 ];
