53783404f0b8615d98cf08741975b11c1c7c6812
Andreas Creten <andreas@madewithlove.com>
2025-02-20 21:14:36 +0100

    Add missing methods to the facade

--- src/Facades/Notification.php
+++ src/Facades/Notification.php
@@ -9,6 +9,8 @@ use Illuminate\Support\Facades\Facade;
  * @method static static event(string $event)
  * @method static static message(string $body)
  * @method static static reference(string $reference)
+ * @method static static hasReply(string $placeholder = '')
+ * @method static static addAction(string $label)
  * @method static void show()
  */
 class Notification extends Facade
