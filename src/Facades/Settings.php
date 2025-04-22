5a2d6522c07912e19e1bee620e2dc1c9ba68b2a2
AJ Meireles <alvaro.meireles@live.com>
2025-04-22 13:48:04 -0300

    Added new methods to Settings class and updated typehints

--- src/Facades/Settings.php
+++ src/Facades/Settings.php
@@ -7,12 +7,15 @@ use Illuminate\Support\Facades\Facade;
 /**
  * @method static void set(string $key, $value)
  * @method static mixed get(string $key, $default = null)
+ * @method static bool has(string $key)
  * @method static void forget(string $key)
  * @method static void clear()
+ *
+ * @see \Native\Laravel\Settings
  */
 class Settings extends Facade
 {
-    protected static function getFacadeAccessor()
+    protected static function getFacadeAccessor(): string
     {
         return \Native\Laravel\Settings::class;
     }
--- src/Settings.php
+++ src/Settings.php
@@ -8,6 +8,13 @@ class Settings
 {
     public function __construct(protected Client $client) {}
 
+    /**
+     * Set a value in the settings using the provided key.
+     *
+     * @param string $key
+     * @param $value
+     * @return void
+     */
     public function set(string $key, $value): void
     {
         $this->client->post('settings/'.$key, [
@@ -15,21 +22,48 @@ class Settings
         ]);
     }
 
+    /**
+     * Retrieve a value from the settings using the provided key.
+     *
+     * @param string $key
+     * @param mixed|null $default
+     *
+     * @return mixed
+     */
     public function get(string $key, $default = null): mixed
     {
         return $this->client->get('settings/'.$key)->json('value') ?? $default;
     }
 
+    /**
+     * Determine if a value exists in the settings for the provided key.
+     *
+     * @param string $key
+     *
+     * @return bool
+     */
     public function has(string $key): bool
     {
         return $this->client->get('settings/'.$key)->json('value') !== null;
     }
 
+    /**
+     * Remove a value from the settings using the provided key.
+     *
+     * @param string $key
+     *
+     * @return void
+     */
     public function forget(string $key): void
     {
         $this->client->delete('settings/'.$key);
     }
 
+    /**
+     * Clear all settings by deleting them from the storage.
+     *
+     * @return void
+     */
     public function clear(): void
     {
         $this->client->delete('settings/');
