11505751c047306b154daa44222be5e69d266b92
WINBIGFOX <winbigfox@t-online.de>
2025-05-03 23:37:02 +0200

    Add support for additional app icon formats (#216)

--- src/Traits/InstallsAppIcon.php
+++ src/Traits/InstallsAppIcon.php
@@ -8,6 +8,10 @@ trait InstallsAppIcon
     {
         @copy(public_path('icon.png'), __DIR__.'/../../resources/js/build/icon.png');
         @copy(public_path('icon.png'), __DIR__.'/../../resources/js/resources/icon.png');
+        @copy(public_path('icon.ico'), __DIR__.'/../../resources/js/build/icon.ico');
+        @copy(public_path('icon.ico'), __DIR__.'/../../resources/js/resources/icon.ico');
+        @copy(public_path('icon.icns'), __DIR__.'/../../resources/js/build/icon.icns');
+        @copy(public_path('icon.icns'), __DIR__.'/../../resources/js/resources/icon.icns');
         @copy(public_path('IconTemplate.png'), __DIR__.'/../../resources/js/resources/IconTemplate.png');
         @copy(public_path('IconTemplate@2x.png'), __DIR__.'/../../resources/js/resources/IconTemplate@2x.png');
     }
