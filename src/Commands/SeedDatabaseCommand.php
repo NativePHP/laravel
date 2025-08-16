abd8e3af69b85bf341e66b98c436b75199a3fb20
gwleuverink <willem@leuver.ink>
2025-08-16 08:42:15 +0200

    make input symmetric with laravel seed command

--- src/Commands/SeedDatabaseCommand.php
+++ src/Commands/SeedDatabaseCommand.php
@@ -5,6 +5,8 @@ namespace Native\Laravel\Commands;
 use Illuminate\Database\Console\Seeds\SeedCommand as BaseSeedCommand;
 use Native\Laravel\NativeServiceProvider;
 use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
 
 #[AsCommand(
     name: 'native:seed',
@@ -14,10 +16,18 @@ class SeedDatabaseCommand extends BaseSeedCommand
 {
     protected $signature = 'native:seed';
 
+    protected function configure(): void
+    {
+        parent::configure();
+
+        $this->addArgument('class', mode: InputArgument::OPTIONAL, description: 'The class name of the root seeder');
+        $this->addOption('class', mode: InputOption::VALUE_OPTIONAL, description: 'The class name of the root seeder', default: 'Database\\Seeders\\DatabaseSeeder');
+    }
+
     public function handle()
     {
-        $this->addOption('database', default: null);
-        $this->addArgument('class', default: 'DatabaseSeeder');
+        // Add the database option here so it won't show up in `--help`
+        $this->addOption('database', mode: InputOption::VALUE_REQUIRED, default: 'nativephp');
 
         (new NativeServiceProvider($this->laravel))->rewriteDatabase();
 
