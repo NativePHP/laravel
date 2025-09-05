905e071e31dcffb71df261d5c748518015648530
gwleuverink <willem@leuver.ink>
2025-09-05 12:53:30 +0200

    replace with original ts files

--- resources/js/libs/menubar/index.ts
+++ resources/js/libs/menubar/index.ts
@@ -5,14 +5,23 @@
  * import { menubar } from 'menubar';
  * ```
  */
+
+/** */
+
+import { app } from 'electron';
+
 import { Menubar } from './Menubar.js';
 import type { Options } from './types.js';
+
 export * from './util/getWindowPosition.js';
 export { Menubar };
+
 /**
  * Factory function to create a menubar application
  *
  * @param options - Options for creating a menubar application, see
  * {@link Options}
  */
-export declare function menubar(options?: Partial<Options>): Menubar;
+export function menubar(options?: Partial<Options>): Menubar {
+  return new Menubar(app, options);
+}
---
+++ resources/js/libs/menubar/util/cleanOptions.ts
@@ -0,0 +1,67 @@
+/**
+ * @ignore
+ */
+
+/** */
+
+import path from 'path';
+import url from 'url';
+import { app } from 'electron';
+
+import type { Options } from '../types.js';
+
+const DEFAULT_WINDOW_HEIGHT = 400;
+const DEFAULT_WINDOW_WIDTH = 400;
+
+/**
+ * Take as input some options, and return a sanitized version of it.
+ *
+ * @param opts - The options to clean.
+ * @ignore
+ */
+export function cleanOptions(opts?: Partial<Options>): Options {
+  const options: Partial<Options> = { ...opts };
+
+  if (options.activateWithApp === undefined) {
+    options.activateWithApp = true;
+  }
+  if (!options.dir) {
+    options.dir = app.getAppPath();
+  }
+  if (!path.isAbsolute(options.dir)) {
+    options.dir = path.resolve(options.dir);
+  }
+  // Note: options.index can be `false`
+  if (options.index === undefined) {
+    options.index = url.format({
+      pathname: path.join(options.dir, 'index.html'),
+      protocol: 'file:',
+      slashes: true,
+    });
+  }
+  options.loadUrlOptions = options.loadUrlOptions || {};
+
+  options.tooltip = options.tooltip || '';
+
+  // `icon`, `preloadWindow`, `showDockIcon`, `showOnAllWorkspaces`,
+  // `showOnRightClick` don't need any special treatment
+
+  // Now we take care of `browserWindow`
+  if (!options.browserWindow) {
+    options.browserWindow = {};
+  }
+
+  // Set width/height on options to be usable before the window is created
+  options.browserWindow.width =
+    // Note: not using `options.browserWindow.width || DEFAULT_WINDOW_WIDTH` so
+    // that users can put a 0 width
+    options.browserWindow.width !== undefined
+      ? options.browserWindow.width
+      : DEFAULT_WINDOW_WIDTH;
+  options.browserWindow.height =
+    options.browserWindow.height !== undefined
+      ? options.browserWindow.height
+      : DEFAULT_WINDOW_HEIGHT;
+
+  return options as Options;
+}
